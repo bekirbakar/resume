30191a203b61a3c139b01d01a51712e35848d03c
bekirbakar <bekirbakar@icloud.com>
2023-04-08 01:20:35 +0300

    Script to clean intermediate, arbitrary file.

---
+++ clean.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# File extensions to remove
+EXTENSIONS=(
+    "*.aux"
+    "*.bbl"
+    "*.blg"
+    "*.fdb_latexmk"
+    "*.fls"
+    "*.idx"
+    "*.ilg"
+    "*.ind"
+    "*.lof"
+    "*.log"
+    "*.lot"
+    "*.nav"
+    "*.out"
+    "*.snm"
+    "*.synctex.gz"
+    "*.toc"
+    "*.xdv"
+    "*.bcf"
+    "*.cut"
+    "*.run.xml"
+)
+
+# Remove files with specified extensions.
+for ext in "${EXTENSIONS[@]}"; do
+    find . -type f -name "$ext" -exec rm {} +
+done
+
+echo "Intermediate and cache files have been deleted."
